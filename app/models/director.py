ca0953f7e6c90b14ca0a4e07616c1fb727e5e00d
Baesak <cornovolenkobogdan@gmail.com>
2022-09-17 19:35:01 +0300

    removed unnecessary folders

--- app/models/director.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-
-class Director(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.VARCHAR(255))
-    last_name = db.Column(db.VARCHAR(255))
-    age = db.Column(db.Integer, db.CheckConstraint("100>age AND age>0"))
--- app/models/genre.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-
-class Genre(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    genre = db.Column(db.VARCHAR(255), unique=True)

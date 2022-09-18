fcbfead93dde58dd5eeab30967acaf1e9f1c4d66
Baesak <cornovolenkobogdan@gmail.com>
2022-09-18 12:26:53 +0300

    removed some garbage

--- app/config.py
+++
@@ -1,8 +0,0 @@
-
-POSTGRES_USER = "postgres"
-POSTGRES_PASSWORD = "postgres_password"
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/films"
-
